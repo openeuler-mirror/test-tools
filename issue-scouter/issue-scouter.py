95433459d7f913c74f501bee5e863fa8d5cb3230
lemon.higgins <lemon.higgins@aliyun.com>
2020-11-09 03:27:38 +0000

    add docstring

--- issue-scouter/issue-scouter.py
+++ issue-scouter/issue-scouter.py
@@ -1,18 +1,39 @@
 # -*- coding: utf-8 -*-
-# @Author : lemon-higgins
-# @Date   : 2020-08-11 10:55:52
-# @Email  : lemon.higgins@aliyun.com
-# @License: Mulan PSL v2
-# @Desc   :
+
+# Copyright (c) 2020. Huawei Technologies Co.,Ltd.ALL rights reserved.
+# This program is licensed under Mulan PSL v2.
+# You can use it according to the terms and conditions of the Mulan PSL v2.
+#          http://license.coscl.org.cn/MulanPSL2
+# THIS PROGRAM IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+# EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+# MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+# See the Mulan PSL v2 for more details.
+####################################
+# @Author    	:   lemon.higgins
+# @Contact   	:   lemon.higgins@aliyun.com
+# @Date      	:   2020-04-09 09:39:43
+# @License   	:   Mulan PSL v2
+# @Version   	:   1.0
+# @Desc      	:   Public function
+#####################################
 
 
-from logging import INFO
-from urllib import request
 from lxml import etree
-import logging, time, pandas, os, yaml
+import urllib
+import logging
+import time
+import pandas
+import os
+import yaml
 
 
-class crawl_issue(object):
+class issueScouter(object):
+    """
+    从github上,爬去自己关注的仓库的相关issue.
+    Args:
+        object ([type]): [account, repository, platform_url]
+    """
+
     def __init__(self, account, repository, platform_url):
         self.account = account
         self.repository = repository
@@ -21,28 +42,28 @@ class crawl_issue(object):
             self.platform_url + self.account + "/" + self.repository + "/" + "issues"
         )
         logging.basicConfig(
-            level=INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+            level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
         )
 
     def check_url(self):
         """
         docstring
         """
-        with request.urlopen(self.platform_url) as url:
+        with urllib.request.urlopen(self.platform_url) as url:
             if url.status is not 200:
                 logging.error(
                     "Unable to connect to the github website, please check your network."
                 )
                 exit(1)
 
-        with request.urlopen(self.platform_url + self.account) as url:
+        with urllib.request.urlopen(self.platform_url + self.account) as url:
             if url.status is not 200:
                 logging.error(
                     "The user(" + self.account + ") can't be found on github."
                 )
                 exit(1)
 
-        with request.urlopen(
+        with urllib.request.urlopen(
             self.platform_url + self.account + "/" + self.repository
         ) as url:
             if url.status is not 200:
@@ -56,9 +77,17 @@ class crawl_issue(object):
                 exit(1)
 
     def crawl_issues_id(self, issue_status):
+        """
+        从自己关注的仓库地址中获取issue的id
+        Args:
+            issue_status (str): [issue状态]
+
+        Returns:
+            [list]: [返回issue的id集合]
+        """
         issues_id = []
 
-        with request.urlopen(
+        with urllib.request.urlopen(
             self.platform_url + self.account + "/" + self.repository + "/" + "issues"
         ) as req:
             if req.status is not 200:
@@ -90,7 +119,7 @@ class crawl_issue(object):
                 + issue_status
                 + "+is%3Aissue"
             )
-            page_req = request.urlopen(page_url)
+            page_req = urllib.request.urlopen(page_url)
             page_html = etree.HTML(page_req.read().decode())
             page_issue_id = page_html.xpath(
                 '//*[@id="js-repo-pjax-container"]//div[@aria-label="Issues"]//div/@id'
@@ -102,6 +131,12 @@ class crawl_issue(object):
         return issues_id
 
     def crawl_issues_info(self, issues_id):
+        """
+        根据issue的id号,获取每个issue的详细信息
+
+        Args:
+            issues_id (list): [所有issue的id号形成的集合]
+        """
         (
             account_list,
             repository_list,
@@ -118,7 +153,7 @@ class crawl_issue(object):
 
             issue_info = [self.account, self.repository, issue_id]
             issue_url = self.issues_url + "/" + issue_id
-            issue_req = request.urlopen(issue_url)
+            issue_req = urllib.request.urlopen(issue_url)
             issue_html = etree.HTML(issue_req.read().decode())
 
             issue_title = (
@@ -170,7 +205,9 @@ class crawl_issue(object):
 
 
 if __name__ == "__main__":
-    with open(os.path.split(os.path.realpath(__file__))[0] + "/issue-scouter.yaml", "r") as f:
+    with open(
+        os.path.split(os.path.realpath(__file__))[0] + "/issue-scouter.yaml", "r"
+    ) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
 
     platform_url = data.get("platform").get("url")
@@ -180,7 +217,7 @@ if __name__ == "__main__":
         account = obj.get("account")
         repository = obj.get("repository")
 
-        issues = crawl_issue(account, repository, platform_url)
+        issues = issueScouter(account, repository, platform_url)
 
         issues.check_url()
 
@@ -188,4 +225,3 @@ if __name__ == "__main__":
 
         if len(issues_id) is not 0:
             issues.crawl_issues_info(issues_id)
-
