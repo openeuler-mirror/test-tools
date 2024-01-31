82f333b318c44691df179e7acb55d752fa1537c9
ding-jiao <15829797643@163.com>
2024-01-31 17:41:59 +0800

    add case

---
+++ Aops_Auto_Test/Aops_Api_Auto_Test/test_case/aops-apollo/test_upload_parse_advisory.py
@@ -0,0 +1,35 @@
+import os
+import pytest
+from Aops_Api_Auto_Test.config import conf
+from Aops_Api_Auto_Test.utils.AssertUtil import AssertUtil
+from Aops_Api_Auto_Test.utils.LogUtil import my_log
+from Aops_Api_Auto_Test.utils.MysqlUtil import ql
+from Aops_Api_Auto_Test.utils.YamlUtil import Yaml
+from Aops_Api_Auto_Test.api.aops_apollo import ApiApollo
+
+data_file = os.path.join(conf.get_data_path(), "aops-apollo", "upload_parse_advisory.yaml")
+log = my_log()
+
+
+class TestUploadParseAdvisory:
+
+    @staticmethod
+    def setup_class():
+        log.info("准备测试套依赖数据")
+        ApiApollo()
+
+    @pytest.mark.parametrize('test_data', Yaml(data_file).data())
+    def test_upload_parse_advisory(self, test_data):
+        test_data = Yaml(conf.get_common_yaml_path()).replace_yaml(test_data)
+        log.info("test_data: {}".format(test_data))
+        ql.connect()
+        before_count = ql.fetchall("select count(*) from parse_advisory_record")[0]["count(*)"]
+        res = ApiApollo().upload_parse_advisory(test_data['data'])
+        if res:
+            assert_res = AssertUtil()
+            assert_res.assert_code(res["body"]["code"], test_data["validate"]["code"])
+            assert_res.assert_label(res["body"]["label"], test_data["validate"]["label"])
+            assert_res.assert_message(res["body"]["message"], test_data["validate"]["message"])
+            if res["body"]["code"] == '200':
+                assert_res.assert_database(test_data["validate"]["sql"], before_count + 1)
+
--- Aops_Auto_Test/Aops_Api_Auto_Test/test_case/aops-zeus/test_set_metric_status.py
+++ Aops_Auto_Test/Aops_Api_Auto_Test/test_case/aops-zeus/test_set_metric_status.py
@@ -3,41 +3,38 @@ import os
 import pytest
 from Aops_Api_Auto_Test.api.aops_zeus import ApiZeus
 from Aops_Api_Auto_Test.config import conf
-from Aops_Api_Auto_Test.utils.AssertUtil import AssertUtil
-from Aops_Api_Auto_Test.utils.LogUtil import my_log
-from Aops_Api_Auto_Test.utils.YamlUtil import Yaml
 from Aops_Api_Auto_Test.query.aops_zeus import QueryDataBase
 from Aops_Api_Auto_Test.test_case.setup import CreateData
+from Aops_Api_Auto_Test.utils.LogUtil import my_log
+from Aops_Api_Auto_Test.utils.AssertUtil import AssertUtil
+from Aops_Api_Auto_Test.utils.YamlUtil import Yaml
 
-data_file = os.path.join(conf.get_data_path(), "aops-zeus", "register_host.yaml")
+data_file = os.path.join(conf.get_data_path(), "aops-zeus", "set_metric_status.yaml")
 log = my_log()
-
-
-class TestRegisterHost:
+class TestSetMetricStatus:
 
     @staticmethod
     def setup_class():
         log.info("准备测试套依赖数据")
-        CreateData().get_group_name()
+        CreateData().get_host_info()
 
     @staticmethod
     def teardown_class():
         log.info("清理当前测试套数据")
-        QueryDataBase().delete_host(host_ip)
+        QueryDataBase().delete_host(Yaml(conf.get_common_yaml_path()).data()['host_ip'])
         QueryDataBase().delete_host_group(Yaml(conf.get_common_yaml_path()).data()['host_group_name'])
         Yaml(conf.get_common_yaml_path()).clear_yaml()
 
     @pytest.mark.parametrize('test_data', Yaml(data_file).data())
-    def test_register_host(self, test_data):
+    def test_set_metric_status(self, test_data):
         test_data = Yaml(conf.get_common_yaml_path()).replace_yaml(test_data)
         log.info("test_data: {}".format(test_data))
         data = test_data["data"]
-        global host_ip
-        host_ip = data['host_ip']
-        res = ApiZeus().register_host(data)
+        res = ApiZeus().set_metric_status(data)
+        log.info("res: {}".format(res))
         assert_res = AssertUtil()
-        assert_res.assert_code(res["body"]["code"], test_data["validate"]["code"])
+        assert_res.assert_code(res['body']["code"], test_data["validate"]["code"])
         assert_res.assert_label(res["body"]["label"], test_data["validate"]["label"])
         assert_res.assert_message(res["body"]["message"], test_data["validate"]["message"])
-        if res["body"]["code"] == '200':
-            assert_res.assert_database(test_data["validate"]['sql'], 1)
+        if res['body']["code"] == 200:
+            assert_res.assert_data(res["body"]["data"], test_data["validate"]["data"])
