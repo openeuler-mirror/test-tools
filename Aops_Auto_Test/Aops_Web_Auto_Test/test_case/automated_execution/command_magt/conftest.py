9ee3b062da7634acdb35d1e873fed3027ed71f76
zjl_long <154542010@qq.com>
2025-01-14 15:45:05 +0800

    add command_magt/test_add_command.py

---
+++ Aops_Auto_Test/Aops_Web_Auto_Test/test_case/automated_execution/command_magt/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from Aops_Web_Auto_Test.common.readconfig import ini
+from Aops_Web_Auto_Test.page_object.user_magt import UserMagtPage
+
+
+@pytest.fixture(scope='module', autouse=True)
+def user_login(drivers):
+    """用户登录"""
+    user = UserMagtPage(drivers)
+    user.get_url(ini.url)
+    user.user_login(ini.user, ini.password)
