f9f8494194d35edf94d4072297c8f6f4c9fd65e3
zjl_long <154542010@qq.com>
2025-01-26 16:14:09 +0800

    add aops testcase command_magt/test_add_task.py

--- Aops_Auto_Test/Aops_Web_Auto_Test/test_case/automated_execution/command_magt/test_add_command.py
+++ Aops_Auto_Test/Aops_Web_Auto_Test/test_case/automated_execution/command_magt/test_add_command.py
@@ -17,75 +17,86 @@ def command(drivers):
 
 class TestAddCommand:
     @pytest.fixture(scope='function', autouse=True)
-    def prepare_and_clean_data(self, drivers, command):
+    def prepare_and_clean_data(self, command):
         command.enter_command_magt_page()
-        self.is_cancel = None
+        self.need_cancel = 0
         self.command_name = None
         yield
         try:
-            if self.is_cancel:
+            if self.need_cancel:
                 command.click_cancel_button()
             if self.command_name:
                 command.delete_command(self.command_name)
         except (NameError, TimeoutException):
             pass
 
-    def test_add_command_001_invalid_command_name(self, drivers, command):
-        self.is_cancel = True
+    def test_add_command_001_invalid_command_name(self, command):
+        """
+        新建命令 - 校验命令名称
+        """
+        self.need_cancel = 1
         characters = string.ascii_letters + string.digits + ''.join(set(string.punctuation) - set("><"))
         # 命令名称为空
         command_name = ''
         timeout = generate_random_number(1, 86400)
         command_content = generate_content(999)
         command.add_command(command_name, timeout, command_content)
-        assert "请输入命令名称" in command.item_explain_error_info("命令名称")
+        assert command.get_item_explain_error("form_item_command_name") in ("请输入命令名称", "Please enter the command name")
         command.click_cancel_button()
 
         # 命令名称长度超过128个字符
         length = generate_random_number(129, 999)
         command_name = ''.join(random.choices(characters, k=length))
         command.add_command(command_name, timeout, command_content)
-        assert "命令名称长度不超过128个字符" in command.item_explain_error_info("命令名称")
+        assert command.get_item_explain_error("form_item_command_name") in ("命令名称长度不超过128个字符", "The command name cannot exceed 128")
 
-    def test_add_command_002_invalid_command_timeout(self, drivers, command):
-        self.is_cancel = True
+    def test_add_command_002_invalid_command_timeout(self, command):
+        """
+        新建命令 - 校验超时时间
+        """
+        self.need_cancel = 1
         # 超时时间为空
         command_name = create_new_name("command")
         timeout = ''
         command_content = generate_content(999)
         command.add_command(command_name, timeout, command_content)
-        assert "请输入超时时间" in command.item_explain_error_info("超时时间")
+        assert command.get_item_explain_error("form_item_timeout") in ("请输入超时时间", "Please enter the timeout")
         command.click_cancel_button()
 
         # 超时时间为0
         timeout = 0
         command.add_command(command_name, timeout, command_content)
-        assert "超时时间需在1-86400之间" in command.item_explain_error_info("超时时间")
+        assert command.get_item_explain_error("form_item_timeout") in ("超时时间需在1-86400之间", "Timeout must be between 1 and 86400")
         command.click_cancel_button()
 
         # 超时时间大于86400
         timeout = generate_random_number(86401, 99999)
         command.add_command(command_name, timeout, command_content)
-        assert "超时时间需在1-86400之间" in command.item_explain_error_info("超时时间")
+        assert command.get_item_explain_error("form_item_timeout") in ("超时时间需在1-86400之间", "Timeout must be between 1 and 86400")
 
-    def test_add_command_003_invalid_command_content(self, drivers, command):
-        self.is_cancel = True
+    def test_add_command_003_invalid_command_content(self, command):
+        """
+        新建命令 - 校验命令内容
+        """
+        self.need_cancel = 1
         # characters = string.ascii_letters + string.digits + ''.join(set(string.punctuation) - set("><"))
         # 命令内容为空
         command_name = create_new_name("command")
         timeout = generate_random_number(1, 86400)
         command_content = ''
         command.add_command(command_name, timeout, command_content)
-        assert "请输入命令内容" in command.item_explain_error_info("命令内容")
+        assert command.get_item_explain_error("form_item_content") in ("请输入命令内容", "Please enter the command content")
 
         # # 命令内容长度超过65535个字符
         # length = generate_random_number(65536, 99999)
         # command_content = ''.join(random.choices(characters, k=length))
         # command.add_command(command_name, timeout, command_content)
-        # assert "命令内容长度不超过65535个字符" in command.item_explain_error_info("命令内容")
+        # assert command.get_item_explain_error("form_item_content") in ("命令内容长度不超过65535个字符")
 
-    def test_add_command_004_minimum_value(self, drivers, command):
-        """有效边界值校验，输入最短有效内容"""
+    def test_add_command_004_minimum_value(self, command):
+        """
+        新建命令 - 有效边界值校验，输入最短有效内容
+        """
         characters = string.ascii_letters + string.digits
         # 命令名称长度等于1个字符
         command_name = ''.join(random.choices(characters, k=1))
@@ -96,11 +107,13 @@ class TestAddCommand:
         command.add_command(command_name, timeout, command_content)
         command.refresh()
         self.command_name = command_name
-        new_command_name = command.replace_locator_text(command_elem['command_list'], command_name)
-        assert command.find_element(new_command_name)
+        new_loc = command.replace_locator_text(command_elem['command_list'], command_name)
+        assert command.find_element(new_loc)
 
-    def test_add_command_005_maximum_value(self, drivers, command):
-        """有效边界值校验，输入最长有效内容"""
+    def test_add_command_005_maximum_value(self, command):
+        """
+        新建命令 - 有效边界值校验，输入最长有效内容
+        """
         characters = string.ascii_letters + string.digits
         # 命令名称长度等于128个字符
         command_name = ''.join(random.choices(characters, k=128))
@@ -111,32 +124,36 @@ class TestAddCommand:
         command.add_command(command_name, timeout, command_content)
         command.refresh()
         self.command_name = command_name
-        new_command_name = command.replace_locator_text(command_elem['command_list'], command_name)
-        assert command.find_element(new_command_name)
+        new_loc = command.replace_locator_text(command_elem['command_list'], command_name)
+        assert command.find_element(new_loc)
 
-    def test_add_command_006_exist_data(self, drivers, command):
-        """添加已经存在的命令"""
-        self.is_cancel = True
+    def test_add_command_006_exist_data(self, command):
+        """
+        新建命令 - 添加已经存在的命令
+        """
+        self.need_cancel = 1
         command_name = create_new_name("command")
         timeout = generate_random_number(1, 86400)
         command_content = generate_content(999)
         command.add_command(command_name, timeout, command_content)
         command.refresh()
         self.command_name = command_name
-        new_command_name = command.replace_locator_text(command_elem['command_list'], command_name)
-        assert command.find_element(new_command_name)
+        new_loc = command.replace_locator_text(command_elem['command_list'], command_name)
+        assert command.find_element(new_loc)
         command.add_command(command_name, timeout, command_content)
         assert command.get_top_right_notice_text() == "data has existed"
 
-    def test_add_command_007_cancel_add_command(self, drivers, command):
-        """取消添加命令"""
+    def test_add_command_007_cancel_add_command(self, command):
+        """
+        新建命令 - 取消添加命令
+        """
         command_name = create_new_name("command")
         timeout = generate_random_number(1, 86400)
         command_content = generate_content(999)
         command.add_command(command_name, timeout, command_content, action='cancel')
         command.refresh()
-        new_command_name = command.replace_locator_text(command_elem['command_list'], command_name)
-        assert not command.find_element(new_command_name)
+        new_loc = command.replace_locator_text(command_elem['command_list'], command_name)
+        assert not command.find_element(new_loc)
 
 
 if __name__ == '__main__':
--- Aops_Auto_Test/Aops_Web_Auto_Test/test_case/automated_execution/command_magt/test_add_task.py
+++ Aops_Auto_Test/Aops_Web_Auto_Test/test_case/automated_execution/command_magt/test_add_task.py
@@ -18,7 +18,7 @@ def command(drivers):
 
 class TestAddTask:
     @pytest.fixture(scope='function', autouse=True)
-    def prepare_and_clean_data(self, drivers, command):
+    def prepare_and_clean_data(self, command):
         command.enter_command_magt_page()
         self.command_name = create_new_name("command")
         timeout = generate_random_number(1, 86400)
@@ -26,11 +26,14 @@ class TestAddTask:
         command.add_command(self.command_name, timeout, command_content)
         command.refresh()
         command.click_element(command_elem['command_execute'])
-        self.is_cancel = None
+        self.need_cancel = 0
         self.task_name = None
+        self.host_group1 = ini._get("CLUSTER.GROUP1", "CLUSTER_NAME1.GROUP1")
+        self.host1 = ini._get("CLUSTER.GROUP1.HOST", "CLUSTER_NAME1.GROUP1.HOSTNAME1")
+        self.host_group0 = ini._get("CLUSTER.GROUP1", "CLUSTER_NAME1.GROUP0")
         yield
         try:
-            if self.is_cancel:
+            if self.need_cancel:
                 command.click_cancel_button()
             command.delete_command(self.command_name)
             if self.task_name:
@@ -38,40 +41,141 @@ class TestAddTask:
         except (NameError, TimeoutException):
             pass
 
-    def test_add_task_001_invalid_task_name(self, drivers, command):
-        self.is_cancel = True
+    def test_add_task_001_invalid_task_name(self, command):
+        """
+        新建任务 - 校验任务名
+        """
+        self.need_cancel = 1
         characters = string.ascii_letters + string.digits + ''.join(set(string.punctuation) - set("><"))
         # 任务名称为空
         task_name = ''
-        command.add_task(task_name, ini._get("CLUSTER.GROUP1", "CLUSTER_NAME1.GROUP1"), ini._get("CLUSTER.GROUP1.HOST", "CLUSTER_NAME1.GROUP1.HOSTNAME1"), self.command_name)
-        assert "请输入任务名称" in command.item_explain_error_info("任务名称")
+        command.add_task_input_task_name(task_name)
+        command.select_host_group(self.host_group1)
+        command.select_host(self.host1)
+        command.select_command(self.command_name)
+        command.add_task()
+        assert command.get_item_explain_error("form_item_task_name") in ("请输入任务名称", "Please enter the task name")
         command.click_cancel_button()
 
         # 任务名称长度超过128个字符
         length = generate_random_number(129, 999)
         task_name = ''.join(random.choices(characters, k=length))
-        command.add_task(task_name, ini._get("CLUSTER.GROUP1", "CLUSTER_NAME1.GROUP1"), ini._get("CLUSTER.GROUP1.HOST", "CLUSTER_NAME1.GROUP1.HOSTNAME1"),  self.command_name)
-        assert "任务名称不能超过128字符" in command.item_explain_error_info("任务名称")
+        command.add_task_input_task_name(task_name)
+        command.select_host_group(self.host_group1)
+        command.select_host(self.host1)
+        command.select_command(self.command_name)
+        command.add_task()
+        assert command.get_item_explain_error("form_item_task_name") in ("任务名称不能超过128字符", "Name cannot exceed 128 characters")
 
-    def test_add_task_002_invalid_host_group(self, drivers, command):
-        self.is_cancel = True
+    def test_add_task_002_invalid_host_group(self, command):
+        """
+        新建任务 - 校验主机组
+        """
+        self.need_cancel = 1
         # 主机组为空
         task_name = create_new_name("task")
-        command.add_task(task_name, '', '', self.command_name)
-        assert "请选择主机组" in command.item_explain_error_info("主机选择")
-        command.click_cancel_button()
+        command.add_task_input_task_name(task_name)
+        command.select_host_group("")
+        command.select_command(self.command_name)
+        command.add_task()
+        assert command.get_item_explain_error("form_item_host_group") in ("请选择主机组", "Please select the host group")
 
         # 选中的主机组下没有添加主机
-        command.add_task(task_name, ini._get("CLUSTER.GROUP1", "CLUSTER_NAME1.GROUP0"), '', self.command_name)
-        # assert "当前选择主机组下没有主机，请选择其他主机组" in command.get_notice_text()
-        assert "No data found in database" in command.get_top_right_notice_text()
+        command.select_host_group(self.host_group0)
+        assert command.get_notice_text() in ("当前选择主机组下没有主机，请选择其他主机组", "There are no hosts in the currently selected host group. Please select another host group.")
+        command.add_task()
+        assert command.get_top_right_notice_text() == "No data found in database"
 
-    def test_add_task_003_invalid_host(self, drivers, command):
-        self.is_cancel = True
-        # 主机为空
+    def test_add_task_003_invalid_host(self, command):
+        """
+        新建任务 - 校验主机（主机为空）
+        """
+        self.need_cancel = 1
         task_name = create_new_name("task")
-        command.add_task(task_name, ini._get("CLUSTER.GROUP1", "CLUSTER_NAME1.GROUP1"), '', self.command_name)
-        assert "请选择主机" in command.item_explain_error_info("主机选择")
+        command.add_task_input_task_name(task_name)
+        command.select_host_group(self.host_group1)
+        command.select_host("")
+        command.select_command(self.command_name)
+        command.add_task()
+        assert command.get_item_explain_error("form_item_host_group") in ("请选择主机", "Please select the host")
+
+    def test_add_task_004_verify_host(self, command):
+        """
+        新建任务 - 校验是否显示主机信息
+        """
+        self.need_cancel = 1
+        # 选中主机后，显示主机信息
+        task_name = create_new_name("task")
+        command.add_task_input_task_name(task_name)
+        command.select_host_group(self.host_group1)
+        command.select_host(self.host1)
+        new_loc = command.replace_locator_text(command_elem['selected_host'], self.host1)
+        assert command.find_element(new_loc)
+
+        # 删除主机，主机信息消失
+        # 方式一：重复选择同一台主机
+        command.select_host(self.host1)
+        new_loc = command.replace_locator_text(command_elem['selected_host'], self.host1)
+        assert not command.find_element(new_loc)
+
+        # 方式二：点击所选主机后的x
+        command.select_host(self.host1)
+        command.delete_selected_content_method_1(self.host1)
+        new_loc = command.replace_locator_text(command_elem['selected_host'], self.host1)
+        assert not command.find_element(new_loc)
+
+        # 方式三：点击所选主机列表操作栏的删除按钮
+        command.select_host(self.host1)
+        command.click_element(command_elem['task_name_label'])
+        command.delete_selected_host_method_2(self.host1)
+        new_loc = command.replace_locator_text(command_elem['selected_host'], self.host1)
+        assert not command.find_element(new_loc)
+
+    def test_add_task_005_invalid_command(self, command):
+        """
+        新建任务 - 校验命令（命令为空）
+        """
+        self.need_cancel = 1
+        task_name = create_new_name("task")
+        command.add_task_input_task_name(task_name)
+        command.select_host_group(self.host_group1)
+        command.select_host(self.host1)
+        command.select_command("")
+        command.add_task()
+        assert command.get_item_explain_error("form_item_command_list") in ("请选择", "Please select")
+
+    def test_add_task_006_verify_command(self, command):
+        """
+        新建任务 - 校验是否显示所选命令信息
+        """
+        self.need_cancel = 1
+        # 选中命令后，显示命令信息
+        task_name = create_new_name("task")
+        command.add_task_input_task_name(task_name)
+        command.select_host_group(self.host_group1)
+        command.select_host(self.host1)
+        command.select_command(self.command_name)
+        new_loc = command.replace_locator_text(command_elem['selected_command'], self.command_name)
+        assert command.find_element(new_loc)
+
+        # 删除命令，命令信息消失
+        # 方式一：重复选择同一条命令
+        command.select_command(self.command_name)
+        new_loc = command.replace_locator_text(command_elem['selected_command'], self.command_name)
+        assert not command.find_element(new_loc)
+
+        # 方式二：点击所选命令后的x
+        command.select_command(self.command_name)
+        command.delete_selected_content_method_1(self.command_name)
+        new_loc = command.replace_locator_text(command_elem['selected_command'], self.command_name)
+        assert not command.find_element(new_loc)
+
+        # 方式三：点击所选命令列表操作栏的删除按钮
+        command.select_command(self.command_name)
+        command.click_element(command_elem['task_name_label'])
+        command.delete_selected_command_method_2(self.command_name)
+        new_loc = command.replace_locator_text(command_elem['selected_command'], self.command_name)
+        assert not command.find_element(new_loc)
 
 
 if __name__ == '__main__':
