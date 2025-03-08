62943f1326e85f03f07e58498c974151b6b4e107
yangshicheng <scyang_zjut@163.com>
2025-03-09 01:44:17 +0800

    test

--- x2openEuler_Web_Auto_Test/main.py
+++ x2openEuler_Web_Auto_Test/main.py
@@ -10,3 +10,4 @@ if __name__ == '__main__':
         os.mkdir(report_path)
     report_file = os.path.join(report_path, report_name)
     pytest.main(["-s", "-v", "-n=3", "test_case/task_upgrade/test_task_upgrade.py", f"--html={report_file}.html"])
+    print("test-devstation")
